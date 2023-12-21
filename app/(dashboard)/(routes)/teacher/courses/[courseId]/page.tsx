cb170d2a6fb009854cc75ddc52a606747deddf0c
marksikaundi <emmanuelmark117@gmail.com>
2023-12-21 17:52:35 +0200

    Add DescriptionForm component to CourseIdPage

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -4,6 +4,7 @@ import { auth } from "@clerk/nextjs";
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
 import { TitleForm } from "./_components/title-form";
+import { DescriptionForm } from "./_components/description-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -55,6 +56,10 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
             initialData={course}
             courseId={course.id}
           />
+          <DescriptionForm
+              initialData={course}
+              courseId={course.id}
+            />
         </div>
       </div>
     </div>
