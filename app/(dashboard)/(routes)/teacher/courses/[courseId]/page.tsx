cc407546de0b0afa2c5bab953a4beb331e7279a6
marksikaundi <emmanuelmark117@gmail.com>
2023-12-21 15:26:49 +0200

    Refactor CourseIdPage component to include authentication and database queries

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,13 +1,31 @@
-const CourseIdPage = ({
-    params
-}: {
-    params: { courseId: string }
-}) => {
-    return ( 
-        <div>
-            <h1>Course Id: {params.courseId} </h1>
-        </div>
-     );
-}
- 
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
+    const { userId } = auth();
+
+    if (!userId) {
+        return redirect("/");
+    }
+
+    const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+  });
+
+    if (!course) {
+        return redirect("/");
+    }
+
+    
+
+  return (
+    <div>
+      <h1>Course Id: {params.courseId} </h1>
+    </div>
+  );
+};
+
 export default CourseIdPage;
