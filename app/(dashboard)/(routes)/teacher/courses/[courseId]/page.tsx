ef8f80d7c7bcac5c53d9526b0ec210686ae6ac32
marksikaundi <emmanuelmark117@gmail.com>
2023-12-21 15:18:18 +0200

    Add Course, Category, and Attachment models

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -0,0 +1,13 @@
+const CourseIdPage = ({
+    params
+}: {
+    params: { courseId: string }
+}) => {
+    return ( 
+        <div>
+            <h1>Course Id: {params.courseId} </h1>
+        </div>
+     );
+}
+ 
+export default CourseIdPage;
